20845b6a6f89506e082e1e93448c58854c2c545e
kevinmk <kevinmk@outlook.com>
2017-09-23 00:17:44 -0500

    thank god, it works.  need to submit...

--- main.py
+++ main.py
@@ -11,26 +11,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
       <!-- create your form here -->
         <form action="/encrypt" method="post">
             <b>Rotate by:</b> <input type="text" name="rot" value="0"><br>
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
             <input type="submit">
         </form>
     </body>
@@ -45,11 +45,12 @@ def encrypt():
     message = request.form['text']
     message = str(message)
     encrypted = rotate_string(message, rotation)
-    return '<h1>' + encrypted + '</h1>'
+    return form.format(encrypted)
+    #return '<h1>' + encrypted + '</h1>'
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 
 app.run()
